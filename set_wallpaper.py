d2b942e52e2dbb84fe78d7df25c9bbe1a87f06d5
siwhyatt <siwhyatt@gmail.com>
2024-10-14 07:45:50 +0000

    Modify set_wallpaper to work for any user

--- set_wallpaper.py
+++ set_wallpaper.py
@@ -6,7 +6,6 @@ import random
 
 def set_gnome_wallpaper(picture_path):
     subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format(picture_path), shell=True)
-    ##os.system('feh --bg-scale ' + picture_path)
     
 def get_files(path,ext='*.'):
     matches = []
@@ -15,19 +14,23 @@ def get_files(path,ext='*.'):
             matches.append(os.path.join(root, filename))
     return matches
 
-def set_random_wallpaper(destination_folder):
-    path = f'/home/siwhyatt/Pictures/Wallpapers/{destination_folder}'
+def set_random_wallpaper(path):
     imageExts = ['*.jpeg','*.jpg','*.JPG','*.png','*.PNG']
     files = []
     for ext in imageExts:
         files += get_files(path,ext)
+    print(len(files))
     index = random.randint(0,len(files)-1)
+    print(files[index])
     set_gnome_wallpaper(r'"' + files[index] + '"')
     print('Wallpaper Set!')
 
+
+def main():
+    path = os.path.expanduser("~/Pictures/Wallpapers/Favourites/")
+    set_random_wallpaper(path)
+
+
 if __name__ == '__main__': 
-    
-    destination_folder = "Favourites"
-    
-    set_random_wallpaper(destination_folder)
+    main()
 
